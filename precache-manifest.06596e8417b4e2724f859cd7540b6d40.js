94e79b5b7bbecb0cbc990b08af07441b38bb06a8
minh9 <minh930906@gmail.com>
2020-08-25 13:21:19 +0200

    Updates

---
+++ precache-manifest.06596e8417b4e2724f859cd7540b6d40.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "993318b36bad4e45e817f76d343ea799",
+    "url": "/udemy-robofriend-practice/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/udemy-robofriend-practice/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ac0f95609ea151773a7f",
+    "url": "/udemy-robofriend-practice/static/css/main.24a3c330.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/udemy-robofriend-practice/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/udemy-robofriend-practice/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac0f95609ea151773a7f",
+    "url": "/udemy-robofriend-practice/static/js/main.7bdf4175.chunk.js"
+  },
+  {
+    "revision": "7e7b2835e0d993fcd615",
+    "url": "/udemy-robofriend-practice/static/js/runtime-main.a0ebeab8.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/udemy-robofriend-practice/static/media/SEGA.12a3db8b.woff"
+  }
+]);
